26d96d519154e8467d11bbac332c65f246f25154
Santiago Lillo Macías <santilillomacias@gmail.com>
2026-04-22 19:33:59 +0200

    Remove instructions and clean up code

--- 1-Point inside a polygon/Point_polygon.py
+++ 1-Point inside a polygon/Point_polygon.py
@@ -1,12 +1,3 @@
-"""PRACTICA 1: 28-01-2026
-Instrucciones:
-- Modifica el nombre de archivo para que comience por tus apellidos (ej. HernandezCorbato_p1.py)
-- Trabaja en las funciones "segmentos_se_cortan" (línea 44), "punto_en_poligono" (línea 49)
-- Para comprobar su funcionamiento ve al final del código y ejecuta la comprobación correspondiente
-- Sube el código .py a la tarea del CV al final de la clase
-Actualizada 03/02/26
-"""
-
 import random
 import math
 import numpy as np
