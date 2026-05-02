846a1084e869d00515b15a0cc08aa51be4ff69ea
Santiago Lillo Macías <santilillomacias@gmail.com>
2026-05-02 23:15:18 +0200

    Remove commented instructions and clean code

--- 7-Cloud Triangulation/Cloud_triangulation.py
+++ 7-Cloud Triangulation/Cloud_triangulation.py
@@ -1,12 +1,3 @@
-"""PRACTICA 7: 11-03-2026
-Instrucciones:
-- Modifica el nombre de archivo para que comience por tus apellidos (ej. HernandezCorbato_p7.py)
-- Trabaja en la función triangula_nube (línea 225)
-- En la parte central del código hay varios algoritmos del cálculo de envolventes convexas
-- Para comprobar su funcionamiento ve al final del código y ejecuta la comprobación correspondiente
-- Sube el código .py a la tarea del CV al final de la clase
-"""
-
 import random
 import math
 import numpy as np
